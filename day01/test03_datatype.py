92ad16b50f29bdfea650c80282d9885e2badc743
RiverGang <sory304@naver.com>
2024-01-29 16:16:06 +0900

    자료형까지 학습

---
+++ day01/test03_datatype.py
@@ -0,0 +1,68 @@
+# date : 20240129
+# desc : 자료형
+
+## 기본자료형 - 숫자형, 문자형, 불형, None형...
+## 추가(=복합)자료형 - 리스트형, 튜플형, 딕셔너리, 집합...
+
+## None형 == null (C, C++, C#, Java, SQL)
+## 값은 값인데 아무 것도 지정할 수 없는 값 => None
+
+apple = None
+print(apple)
+
+## 숫자형 - 정수형, 실수형, 진수형
+### 정수
+ten = 10
+hunndred = 100
+temp = -8
+
+### 실수
+pi = 3.141592
+tax_rate = 10.0
+emp_earn_rate = 3.3
+test_val = 2e10
+print(test_val)
+
+### 진수 (2, 8, 16진수)
+bit142 = 0b10001110
+oct9 = 0o11
+hex255 = 0xFF
+print(bit142)
+print(oct9)
+print(hex255)
+
+## 문자형 - 파이썬에는 문자, 문자열 차이가 없음
+greeting = 'Hello!'
+greeting = "Hello!" # '홑따옴표', "쌍따옴표" 모두 문자열 나타냄 동일
+print(greeting)
+multi_str = """Hello 
+I am a programmer. 
+Have a good afternoon."""
+print(multi_str)
+multi_str2 = ('Line1\n'
+              'Line2\n'
+              'Line3\n') # ()를 다 쓰거나, 문장 끝마다 \를 붙이기
+print(multi_str2)
+
+## 불형 (Bool, Boolean)
+isCheck = False
+print(isCheck)
+
+isCheck = True
+print(isCheck)
+
+answer = (3 == 6)
+print(answer)
+
+answer = (3.0 == 3)
+print(answer)
+
+
+## 자료형이 어떤 타입인지 체크
+print(type(apple))
+print(type(hunndred))
+print(type(test_val))
+print(type(hex255))
+print(type(greeting))
+print(type(isCheck))
+
